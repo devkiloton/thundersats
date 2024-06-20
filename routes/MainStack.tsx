5343bd53e5316e562a67e2d412516259e4948c38
devkiloton <dev.kiloton@gmail.com>
2024-06-20 22:40:47 +0200

    Creating main stack

--- routes/MainStack.tsx
+++ routes/MainStack.tsx
@@ -1,12 +1,16 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen } from "../screens/HomeScreen";
+import { useTheme } from "react-native-paper";
+import { MapScreen } from "../screens/MapScreen";
 
 const Stack = createStackNavigator();
 
 export const MainStack = () => {
+  const theme = useTheme();
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Map" component={MapScreen} />
     </Stack.Navigator>
   );
 };
