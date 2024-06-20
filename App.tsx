97d4483a2472afa04a0e0530cc5d9f3735484f14
devkiloton <dev.kiloton@gmail.com>
2024-06-20 19:04:40 +0200

    Implementing react navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Map } from "./components/Map";
+import { MainStack } from "./routes/MainStack";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Map />
+    <NavigationContainer>
+      <MainStack />
+      {/* <Map /> */}
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ routes/MainStack.tsx
@@ -0,0 +1,12 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { HomeScreen } from "../screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+export const MainStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
