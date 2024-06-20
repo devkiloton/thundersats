c705a5725ee3e53c15f3e587ef58c0addc10601c
devkiloton <dev.kiloton@gmail.com>
2024-06-20 18:50:08 +0200

    Creating map component

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,7 +3,22 @@ import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
 
 export const Map = () => {
-  return <MapView provider={PROVIDER_GOOGLE} style={styles.map} />;
+  return (
+    <MapView
+      googleMapId={process.env.GOOGLE_MAP_ID}
+      provider={PROVIDER_GOOGLE}
+      style={styles.map}
+      // customMapStyle={}
+      showsMyLocationButton={true}
+      showsBuildings={true}
+      showsPointsOfInterest={false}
+      showsIndoors={false}
+      showsCompass={false}
+      showsIndoorLevelPicker={false}
+      showsTraffic={false}
+      showsUserLocation
+    />
+  );
 };
 
 const styles = StyleSheet.create({
