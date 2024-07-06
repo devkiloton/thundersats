8a98ca4990a7f45e4a33957937fd1298804082a7
devkiloton <dev.kiloton@gmail.com>
2024-07-06 18:40:11 +0200

    Setting up firebase

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Map } from "./components/Map";
 import { MainStack } from "./routes/MainStack";
 import { NavigationContainer } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./config/firebase-config";
+import { getFirestore } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
 export default function App() {
   return (
