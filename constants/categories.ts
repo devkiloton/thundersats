628efece5f28e224b39a4850a4611fa5fe95e188
devkiloton <dev.kiloton@gmail.com>
2024-07-06 18:41:45 +0200

    Updating categories constant

--- constants/categories.ts
+++ constants/categories.ts
@@ -1,11 +1,14 @@
+// Create the categories array with objects containing the name and icon properties.
 export const categories = [
-  "Food",
-  "Exchange",
-  "Electronics",
-  "Medicines",
-  "Services",
-  "Books",
-  "Home",
-  "Games",
-  "Computers",
+  { name: "Food", icon: "food" },
+  { name: "Exchange", icon: "bitcoin" },
+  { name: "Services", icon: "wrench" },
+  { name: "Electronics", icon: "cellphone" },
+  { name: "Medicines", icon: "pill" },
+  { name: "Books", icon: "book" },
+  { name: "Home", icon: "home" },
+  { name: "Games", icon: "gamepad" },
+  { name: "Computers", icon: "desktop-tower" },
+  { name: "Clothes", icon: "tshirt-crew" },
+  { name: "Other", icon: "dots-horizontal" },
 ];
