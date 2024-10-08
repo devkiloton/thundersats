96d497a74e4d92f147218e6d948f182e2636fb60
devkiloton <dev.kiloton@gmail.com>
2024-10-08 21:55:50 +0200

    Setting up new hook

--- hooks/firebase.ts
+++ hooks/firebase.ts
@@ -1,4 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "../config/firebase-config";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 export const app = () => initializeApp(firebaseConfig);
+
+export const authInit = () =>
+  initializeAuth(app(), {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
