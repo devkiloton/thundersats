e1699898ad73dd55d43a96725032055dd5473593
devkiloton <dev.kiloton@gmail.com>
2024-10-08 21:52:25 +0200

    Setting up background as rn paper

--- App.tsx
+++ App.tsx
@@ -1,15 +1,25 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { MainStack } from "./routes/MainStack";
-import { NavigationContainer } from "@react-navigation/native";
-import { PaperProvider } from "react-native-paper";
-import { app } from "./hooks/firebase";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { PaperProvider, MD3LightTheme } from "react-native-paper";
+import { app, authInit } from "./hooks/firebase";
 
 export default function App() {
   app();
+  authInit();
+
   return (
     <PaperProvider>
-      <NavigationContainer>
+      <NavigationContainer
+        theme={{
+          ...DefaultTheme,
+          colors: {
+            ...DefaultTheme.colors,
+            background: MD3LightTheme.colors.background,
+          },
+        }}
+      >
         <MainStack />
         <StatusBar style="auto" />
       </NavigationContainer>
