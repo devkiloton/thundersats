15d8e76a95afbb9850259cd653438a7d8f8c2ae7
devkiloton <dev.kiloton@gmail.com>
2024-10-10 21:42:54 +0200

    Refactoring homescreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { ParamListBase, useNavigation } from "@react-navigation/native";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { FAB, useTheme } from "react-native-paper";
 import { Text } from "react-native-paper";
 import { categories } from "../constants/categories";
@@ -7,74 +7,80 @@ import { CategoryCarousel } from "../components/CategoryCarousel";
 import { useState } from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { PlaceList } from "../components/PlaceList";
+import { CategoriesEnum } from "../constants/categories";
 
 export const HomeScreen = () => {
   const theme = useTheme();
   const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedCategory, setSelectedCategory] =
+    useState<CategoriesEnum | null>(null);
 
   return (
-    <PlaceList>
-      <View
-        style={{
-          ...styles.container,
-          backgroundColor: theme.colors.background,
-        }}
-      >
-        <View style={styles.header}>
-          <FAB
-            customSize={96}
-            icon="map-marker"
-            mode="flat"
-            size="large"
-            label="Find places"
-            style={{ width: "100%" }}
-            onPress={() => navigation.navigate("Map")}
+    <PlaceList category={selectedCategory}>
+      <>
+        <View
+          style={{
+            ...styles.container,
+            backgroundColor: theme.colors.background,
+          }}
+        >
+          <View style={styles.header}>
+            <FAB
+              customSize={96}
+              icon="map-marker"
+              mode="flat"
+              size="large"
+              label="Find places"
+              style={{ width: "100%" }}
+              onPress={() => navigation.navigate("Map")}
+            />
+          </View>
+          <View style={styles.secondaryHeader}>
+            <FAB
+              customSize={96}
+              icon="heart"
+              mode="flat"
+              size="large"
+              label="Favorites"
+              style={{ flex: 1 }}
+              onPress={() => console.log("Pressed")}
+            />
+            <FAB
+              customSize={96}
+              icon="plus"
+              mode="flat"
+              size="large"
+              label="New place"
+              style={{ flex: 1 }}
+              onPress={() => navigation.navigate("New place")}
+            />
+          </View>
+          <Text variant="headlineLarge" style={{ fontWeight: "bold" }}>
+            Categories
+          </Text>
+          <CategoryCarousel
+            categories={categories}
+            activeCategory={selectedCategory}
+            onChange={setSelectedCategory}
           />
         </View>
-        <View style={styles.secondaryHeader}>
-          <FAB
-            customSize={96}
-            icon="heart"
-            mode="flat"
-            size="large"
-            label="Favorites"
-            style={{ flex: 1 }}
-            onPress={() => console.log("Pressed")}
-          />
-          <FAB
-            customSize={96}
-            icon="plus"
-            mode="flat"
-            size="large"
-            label="New place"
-            style={{ flex: 1 }}
-            onPress={() => navigation.navigate("New place")}
-          />
-        </View>
-        <Text variant="headlineLarge" style={{ fontWeight: "bold" }}>
-          Categories
-        </Text>
-        <CategoryCarousel
-          categories={categories}
-          activeCategory={selectedCategory}
-          onChange={setSelectedCategory}
-        />
-      </View>
+      </>
     </PlaceList>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    gap: 16,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 8,
+    gap: 12,
   },
   header: {
     gap: 16,
   },
   secondaryHeader: {
     flexDirection: "row",
-    gap: 16,
+    gap: 12,
   },
 });
